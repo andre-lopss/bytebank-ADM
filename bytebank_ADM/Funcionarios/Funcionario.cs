b4e2f9c5ab66edbfb4ea084406821a654363ed2f
André Lopes <andrelneres@hotmail.com>
2023-01-11 15:40:30 -0300

    Commit inicial

---
+++ bytebank_ADM/Funcionarios/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ADM.Funcionarios
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario()
+        {
+        }
+
+        public Funcionario(string nome, string cpf, double salario)
+        {
+            Nome = nome;
+            Cpf = cpf;
+            Salario = salario;
+        }
+
+        public double GetBonificacao()
+        {
+            return this.Salario * 0.10;
+        }
+    }
+}
---
+++ bytebank_ADM/Program.cs
@@ -0,0 +1,23 @@
+﻿using bytebank_ADM.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ADM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario Pedro = new Funcionario("Pedro Silva", "08342484507", 2000);
+
+            Console.WriteLine(Pedro.Nome);
+            Console.WriteLine(Pedro.Cpf);
+            Console.WriteLine(Pedro.Salario);
+
+            Console.ReadLine();
+        }
+    }
+}
